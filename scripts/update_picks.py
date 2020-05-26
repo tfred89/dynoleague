d22ff7bf012edb92d00a07b9d0b8c34f0cb57aea
Trent Frederick <trentfrederick@gmail.com>
2020-05-26 15:44:10 -0500

    added script for pick creation

---
+++ scripts/update_picks.py
@@ -0,0 +1,31 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dynoleague.settings")
+
+import django
+django.setup()
+
+from league.models import DraftPick
+
+def create_pick(yr, rd, pk, known):
+    if known:
+        p, c = DraftPick.objects.get_or_create(
+            year=yr,
+            round=rd,
+            pick=pk
+        )
+    else:
+        p, c = DraftPick.objects.get_or_create(
+            year=yr,
+            round=rd
+        ) 
+    if c:
+        print(f'{yr} pick {rd} has been added')
+
+def create_year(yr, known):
+    for rd in range(1,6):
+        for pk in range(1,11):
+            create_pick(yr, rd, pk, known)
+
+if name == '__main__':
+    create_year(2019, True)
+    create_year(2022, False)
